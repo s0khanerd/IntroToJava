429a5e36889d857451aec36a3827c46b2bde5637
s0khanerd <126134985+s0khanerd@users.noreply.github.com>
2023-02-22 20:07:45 -0800

    Create ArithKhan.java

---
+++ ArithKhan.java
@@ -0,0 +1,24 @@
+/*
+ * CS 125-01 - Assignment 1
+ * Topics; Arithmetic, Data Types, Importing modules
+ * Name: Samia Khan - 0545072
+ * Development Environment
+ * Date: 2/14/2023
+ */
+public class ArithKhan {
+	public static void main(String args[]) {
+		int myId=24;
+		int nLet=4;
+		System.out.println("My family name is: Khan");
+		System.out.println("My student id is: 0545072");
+		System.out.println("myId is: 23");
+		System.out.println("nLet is: 4");
+		System.out.println("Expression #1 --> " + (myId + nLet) );
+		System.out.println("Expression #2 --> " + (myId - nLet) );
+		System.out.println("Expression #3 --> " + (myId / 2) );
+		System.out.println("Expression #4 --> " + (myId % 2) );	
+		System.out.println("Expression #5 --> " + (myId + nLet * 2) );
+		System.out.println("Expression #6 --> " + (myId) / (nLet+1100) );	
+	}
+}
+
